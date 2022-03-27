22d7159954517cb4b3b9eb92244dc909d4479884
hubdev04 <76955127+hubdev04@users.noreply.github.com>
2022-03-27 10:47:08 +0530

    after

--- Important folder/index.js
+++ Important folder/index.js
@@ -6,26 +6,33 @@
 // },12*1000);
 
 
-const imagearray=["./flower1.png","./flower2.png","./pinkflower.png"];
-var img1=document.querySelector(".sli1 img");
-img1.src=imagearray[0];
-var img2=document.querySelector(".sli2 img");
-img2.src=imagearray[1];
-var img3=document.querySelector(".sli3 img");
-img3.src=imagearray[2];
-var i=0;
-function func()
-{
+
+
+////for image chnage
+// const imagearray=["./flower1.png","./flower2.png","./pinkflower.png"];
+// var img1=document.querySelector(".sli1 img");
+// img1.src=imagearray[0];
+// var img2=document.querySelector(".sli2 img");
+// img2.src=imagearray[1];
+// var img3=document.querySelector(".sli3 img");
+// img3.src=imagearray[2];
+// var i=0;
+// function func()
+// {
     
-    var prev=(1+i)%(imagearray.length);
-    img1.src=imagearray[prev];
-    var current=(2+i)%(imagearray.length);
-    img2.src=imagearray[current];
-    var next=(3+i)%(imagearray.length);
-    img3.src=imagearray[next];
-    i++;
-}
-setInterval(func,4000);
+//     var prev=(1+i)%(imagearray.length);
+//     img1.src=imagearray[prev];
+//     var current=(2+i)%(imagearray.length);
+//     img2.src=imagearray[current];
+//     var next=(3+i)%(imagearray.length);
+//     img3.src=imagearray[next];
+//     i++;
+// }
+// setInterval(func,4000);
+
+
+
+///for preloader
 // var pre =document.querySelector(".preloadergif");
 // var pre2 =document.querySelector(".pre");
 // function f()
